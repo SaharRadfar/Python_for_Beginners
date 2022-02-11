a699ee6fec004421fd26a1f350ecffe3bbf6b2d9
SaharRadfar <raadfar@gmail.com>
2022-02-11 13:22:05 +1100

    Update Hangman.py
    Apply some changes

--- HangMan/Hangman.py
+++ HangMan/Hangman.py
@@ -1,31 +1,33 @@
 import random
 from words import word_list
 
-def computer_word_list(computer_word):
-    print(computer_word)
-    comp_word_list = []
-    for i in computer_word:
-        comp_word_list.append(i)
-
-    return (comp_word_list)
-
 def hangman():
-    computer_word = random.choice(word_list)
-    word = computer_word_list(computer_word)
-    print("This is Hangman game, I will select a word and let's see if you can guess what it is")
-    print(f"let's start, my word had {len(word)} letters")
+    word = random.choice(word_list).upper()
     user_word = []
     show_word = []
+    used_letter = []
+    print("This is Hangman game, I will select a word and let's see if you can guess what it is")
+    print(f"let's start, my word had {len(word)} letters")
 
+    for i in word:
+        show_word.append("-")
+    
     while set(user_word) != set(word):
-        letter = input ("Guess a letter: ")
-        if letter in word:
+        print (f'You have used following letters {" ".join(used_letter).upper()}')
+        letter = input ("Guess a letter: ").upper()
+        if letter in used_letter:
+            print (f"You have already used {letter} Character, try another one.")
+        
+        elif letter in set(word):
             user_word.append(letter)
+            used_letter.append(letter)
             print (f"Correct, '{letter}' is letter {word.index(letter)+1} in my word")
+
         else:
             print ("nice try, guess again!")
+            used_letter.append(letter)
 
-    print(f"Well done, you did it. my word is {computer_word}")
+    print(f"Well done, you did it. my word is {word}")
 
 
 hangman()
