d4a8c29a40784e77907751784bb0fc1ea16ee030
SaharRadfar <raadfar@gmail.com>
2022-02-11 12:15:35 +1100

    Adding score to RPS
    Adding score to RPS

--- Rock Paper Scissors/RPS.py
+++ Rock Paper Scissors/RPS.py
@@ -12,23 +12,39 @@ def abreviation(argument):
     elif argument == "p":
         return "Paper"
     
-def rps_game(user):
-    computer = random.choice(["r" , "p" , "s"])
+def rps_game(user_score,computer_score):
+    print(f"You: {user_score} - Computer: {computer_score}")
+    while user_score != 5 and computer_score != 5:
+        user = input("What's your choice? Rock(R), Paper(P) or Scissor(S) ").lower()
+        computer = random.choice(["r" , "p" , "s"])
+        
+        while user not in user_input:
+            print (f"You: {user_score} - Computer: {computer_score}")
+            print("Wrong input, try again")
+            user = input("Choose Rock(R), Paper(P) or Scissor(S) ").lower()
 
-    if computer == user:
-        return f"Tie, we both choose {abreviation(user)}"
+        if computer == user:
+            print (f"Tie, we both choose {abreviation(user)}")
+        
+        if winer(computer , user):
+                computer_score += 1
+                print (f"I choose {abreviation(computer)} and you choose {abreviation(user)}")
+                
+        
+        if winer(user , computer):
+            print (f"I choose {abreviation(computer)} and you choose {abreviation(user)}")
+            user_score += 1
     
-    if winer(computer , user):
-            return f"Hurray, I win. I choose {abreviation(computer)} and you choose {abreviation(user)}"
+    if user_score > computer_score :
+        print (f"Oops, I lost the game")
+        print(f"final Score: You: {user_score} - Computer: {computer_score}")
     
-    return (f"Oops, I lost. I choose {abreviation(computer)} and you choose {abreviation(user)}")
+    else:  
+        print (f"Hurray, I won the game")
+        print(f"final Score: You: {user_score} - Computer: {computer_score}")    
 
-
-user = input("Choose Rock(R), Paper(P) or Scissor(S) ").lower()
+user_score = 0
+computer_score = 0
 user_input = ["r" , "s" , "p"]
 
-while user not in user_input:
-    print("Wrong input, try again")
-    user = input("Choose Rock(R), Paper(P) or Scissor(S) ").lower()
-    
-print(rps_game(user))
+rps_game(user_score,computer_score)
