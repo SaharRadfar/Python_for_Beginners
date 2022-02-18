ebce86122038aeaeee606a4bd0cd007263903731
SaharRadfar <raadfar@gmail.com>
2022-02-18 15:07:52 +1100

    Update Handman.py

--- HangMan/Hangman.py
+++ HangMan/Hangman.py
@@ -4,30 +4,34 @@ from words import word_list
 def hangman():
     word = random.choice(word_list).upper()
     user_word = []
-    show_word = []
     used_letter = []
-    print("This is Hangman game, I will select a word and let's see if you can guess what it is")
-    print(f"let's start, my word had {len(word)} letters")
-
-    for i in word:
-        show_word.append("-")
+    show_word = [letter if letter in used_letter else "-" for letter in word]
     
+    print("This is Hangman game, I will select a word and you should guess it.")
+    print (f'Current word is {" ".join(show_word)}')
+
     while set(user_word) != set(word):
-        print (f'You have used following letters {" ".join(used_letter).upper()}')
+        print("")
+        print (f'You have used following letters: {", ".join(used_letter).upper()}')
         letter = input ("Guess a letter: ").upper()
+        
         if letter in used_letter:
             print (f"You have already used {letter} Character, try another one.")
         
         elif letter in set(word):
             user_word.append(letter)
             used_letter.append(letter)
-            print (f"Correct, '{letter}' is letter {word.index(letter)+1} in my word")
-
+            show_word = [letter if letter in used_letter else "-" for letter in word]
+            print (f'Correct. Current word is {" ".join(show_word)}')
+            
         else:
-            print ("nice try, guess again!")
+            print (f'nice try, guess again! Current word is {" ".join(show_word)}')
             used_letter.append(letter)
+            
 
+    print("")
     print(f"Well done, you did it. my word is {word}")
+    print("")
 
 
 hangman()
